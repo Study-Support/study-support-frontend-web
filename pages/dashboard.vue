ad0820abb5649863b8755e90cf056e22e1369b3c
ThuNhu2801 <hoangthithunhu28012001@gmail.com>
2022-10-09 10:11:48 +0700

    base dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,113 +1,183 @@
 <template>
-  <div class="full">
-    <BContainer>
-      <BRow class="row1 m-1">
-        <BCol>
-          <BTabs
-            pills card
-            align="end"
-          >
-            <BTab title="Tuần" active>
-              <StatisticLineChart
-                :chart-type="getConfig('constants.chartType.week')"
-                :title="'tuần'"
-              />
-            </BTab>
-            <BTab title="Tháng">
-              <StatisticLineChart
-                :chart-type="getConfig('constants.chartType.month')"
-                :title="'tháng'"
-              />
-            </BTab>
-          </BTabs>
-        </BCol>
-      </BRow>
-      <BRow class="mt-3">
-        <BCol>
-          <BRow class="mt-3">
-            <h6 class="col">
-              Tài khoản của cả gia đình
-            </h6>
-          </BRow>
-          <BRow class="d-flex justify-content-center row2 m-2">
-            <BCol>
-              <BRow
-                v-for="account in dataAccounts" :key="account.id"
-                class="d-flex justify-content-between account mb-2 "
-              >
-                <p class="col-auto">{{ account.name }}</p>
-                <p class="col-auto">{{ account.amount }}</p>
-              </BRow>
-              <BRow class="row d-flex justify-content-between sum-account mb-3">
-                <p class="col-auto">Tổng</p>
-                <p class="col-auto">{{ sumAccount }}</p>
-              </BRow>
-            </BCol>
-          </BRow>
-        </BCol>
-      </BRow>
-    </BContainer>
+  <div>
+    <BContainer fluid class="top-dashboard">
+      <BContainer class="a">
+        <BRow>
+          <BCol>
+            <BRow class="d-flex justify-content-between">
+              <BCol class="col-auto">
+                <span class="contact"> Contact us on <a href="#">0 800 123 4567</a> or <a href="https://dut.udn.vn/">dut.udn.vn</a></span>
+              </BCol>
+              <ul class="col col-auto d-flex login">
+                <li class="d-flex">
+                  <NuxtLink class="text-decoration-none d-block" to="/exchanges/expense"> Register </NuxtLink>
+                </li>
+                <li class="d-flex">
+                  <NuxtLink class="text-decoration-none d-block" to="/exchanges/expense"> Login </NuxtLink>
+                </li>
+              </ul>
+            </BRow>
+            <BRow class="mt-3 mb-3 d-flex justify-content-between">
+              <BCol class="col-auto">
+                <NuxtLink to="/dashboard">
+                  <!-- <img src="assets/logo_dark.png" alt=""> -->
+                  <h1>Study With Us</h1>
+                </NuxtLink>
+              </BCol>
+              <ul class="col col-auto d-flex menu">
+                <li class="text-decoration-none d-block" :style="{'background-color':'#1dbb90'}">HOMEPAGE</li>
+                <li class="text-decoration-none d-block">GROUPS</li>
+                <li class="text-decoration-none d-block">FIND MENTOR</li>
+                <li class="text-decoration-none d-block">MENTORS</li>
+                <li class="text-decoration-none d-block">NOTIFICATIONS</li>
+              </ul>
+            </BRow>
+            <BRow>
+              <BCol>
+                <span class="horizontal"></span>
+              </BCol>
+            </BRow>
+          </BCol>
+        </BRow>
+        <BRow class="mt-5">
+          <BCol>
+            <img src="assets/laptop.png" alt="" class="laptop">
+          </BCol>
+          <BCol class="ms-5">
+            <BRow class="mb-5">
+              <h2 class="col col-9"> Together we can change the world </h2>
+            </BRow>
+            <BRow class="mb-4">
+              <p class="col col-10"> Study while others are sleeping, work while others are loafing, prepare while others are playing, and dream while others are wishing. </p>
+            </BRow>
+            <BRow>
+              <BCol>
+                <span class="let-start"> Let's get started </span>
+              </BCol>
+            </BRow>
+          </BCol>
+        </BRow>
+      </BContainer>
+  </BContainer>
   </div>
 </template>
 <script setup>
-
+import "@fontsource/love-ya-like-a-sister";
 definePageMeta({
-  middleware: 'authenticated',
+  layout: false,
 });
-const {formatter} = useFormatMoney();
-const {getConfig} = useConfig();
-const {
-  data: dataAccounts,
-  onFetchResponse,
-  get,
-} = useFetchApi({
-  requireAuth: true,
-  disableHandleErrorUnauthorized: false,
-})(
-  '/accounts',
-  {immediate: false},
-);
-const sumAccount = ref(0);
-get().json().execute();
-onFetchResponse(() => {
-  for (let i = 0; i < dataAccounts.value.length; i += 1) {
-    sumAccount.value += dataAccounts.value[i].amount;
-    dataAccounts.value[i].amount = formatter.format(dataAccounts.value[i].amount);
-  }
-  sumAccount.value = formatter.format(sumAccount.value);
-});
+const handleScroll = () => {
+      // Any code to be executed when the window is scrolled
+      this.isUserScrolling = (window.scrollY > 0);
+      console.log('calling handleScroll');
+}
+onMounted(() => {
+  this.handleDebouncedScroll = debounce(this.handleScroll, 100);
+  window.addEventListener('scroll', this.handleDebouncedScroll);
+})
+onBeforeDestroy(() => {
+  window.removeEventListener('scroll', this.handleDebouncedScroll);
+})
 </script>
-
 <style scoped>
-p {
-  margin: 8px;
+* {
+  color: white;
+}
+h1, h2 {
+  font-family: "Love Ya Like A Sister";
+}
+h2 {
+  display: block;
+  min-width: 20px;
+}
+h1 {
+  margin: 0px;
+}
+.top-dashboard {
+  background-image: url("assets/background.png");
+  background-repeat: none;
+  background-size: 100%;
+  min-height:  703px;
 }
 
-.account,
-.sum-account {
-  /* background-color: #E8E8E8; */
-  background-color: rgb(255, 255, 255);
-  border-radius: 10px;
-  box-shadow: 8px 8px 8px -8px rgb(180, 180, 180);
+.top-dashboard {
+    position: relative;
 }
+.top-dashboard:after {
+    content : "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: url("assets/background.jpg");
+    width: 100%;
+    height: 100%;
+    opacity : 0.2;
+    z-index: -1;
+}
+ul.login li a {
+  background-color: #1dbb90;
+  color:white;
+  padding: 5px 15px;
+  margin-right: 5px;
+  border-radius: 0 0 4px 4px;
+  box-shadow: -4px 3px 0px 0px rgb(0 0 0 / 20%);
+  font-size: small;
+}
+span.contact {
+  font-size: small;
+}
+span.contact a {
+  color: rgb(171, 171, 171);
+}
+img {
+  width: 250px;
+}
+ul.menu li {
+  font-size: 13px;
+  font-weight: 700;
+  padding: 0 16px;
+  margin-right: 5px;
+  line-height: 3em;
+  text-transform: uppercase;
+  border-radius: 4px;
+  box-shadow: -4px 3px 0px 0px rgb(0 0 0 / 0%);
+  background-color: transparent;
+}
+ul.menu li:first-child {
+  box-shadow: -4px 3px 0px 0px rgb(0 0 0 / 20%);
+}
+ul.menu li:last-child {
+  margin-right: 0px;
+  padding-right: 0px;
+}
+ul.menu li:hover {
+  box-shadow: -4px 3px 0px 0px rgb(0 0 0 / 20%);
+  background-color: #1dbb90;
+  /* -webkit-transition: background-color 1000ms linear;
+  -ms-transition: background-color 1000ms linear; */
+  transition: background-color 300ms linear;
+}
+.horizontal {
+  height: 0.4em;
+  width: 100%;
+  display: block;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: -4px 3px 0px 0px rgb(0 0 0 / 20%);
+}
+img.laptop {
+  width: 100%;
 
-.sum-account {
-  background-color: rgb(248, 192, 192);
 }
-.full {
-  background-color: #F6F5FA;
-}
-.container {
-  padding-top: 10px;
-}
-.row1 {
-  background-color: rgb(255, 255, 255);
-  border-radius: 10px;
-}
-div >>> button {
-    color: rgb(0, 0, 0);
-  }
-div >>> button.active{
-  color: rgb(255, 143, 143)!important;
+.let-start {
+  display: inline-block;
+  background-color: white;
+  color: rgb(20,154,189);
+  width: 200px;
+  text-align: center;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: -4px 4px 0px 0px rgb(0 0 0 / 20%);
 }
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,65 +1,130 @@
 <template>
-  <div class="full">
-    <BContainer fluid class="container-fluid">
-      <BContainer>
-        <BRow lg="7" class="text-center">
+  <div>
+    <BContainer fluid class="top-dashboard">
+      <BContainer class="a">
+        <BRow>
           <BCol>
-            <h1 class="align-self-left">Đăng nhập</h1>
+            <BRow class="d-flex justify-content-between">
+              <BCol class="col-auto">
+                <BRow>
+                  ĐẠI HỌC ĐÀ NẴNG
+                </BRow>
+                <BRow v-on:scroll="handleScroll">
+                  TRƯỜNG ĐẠI HỌC BÁCH KHOA
+                </BRow>
+              </BCol>
+              <ul class="col col-auto">
+                <li>
+                  <NuxtLink class="" to="/exchanges/expense"> Register </NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink class="" to="/exchanges/expense"> Login </NuxtLink>
+                </li>
+              </ul>
+            </BRow>
+            <BRow>
+              <BCol>
+                TEN UNG DUNG
+              </BCol>
+              <BCol>
+                MENU
+              </BCol>
+            </BRow>
+            <BRow>
+              thanh ngang
+            </BRow>
           </BCol>
         </BRow>
-        <BRow class="d-flex justify-content-center">
-          <BCol lg="7">
-            <form @submit.prevent="submit">
-              <div role="group">
-                <BFormInput
-                  id="input-live"
-                  v-model="requestBody.username"
-                  :state="validationErrorMessages.username === undefined ? null : false"
-                  aria-describedby="input-live-help input-live-feedback"
-                  placeholder="Tên đăng nhập"
-                  trim
-                  required
-                  class="mt-2"
-                />
-                <BFormInvalidFeedback>
-                  <ValidationErrorMessage :messages="validationErrorMessages.username" />
-                </BFormInvalidFeedback>
-              </div>
-              <div role="group">
-                <BFormInput
-                  id="input-live"
-                  v-model="requestBody.password"
-                  :state="validationErrorMessages.password === undefined ? null : false"
-                  aria-describedby="input-live-help input-live-feedback"
-                  placeholder="Mật khẩu"
-                  trim
-                  type="password"
-                  required
-                  class="mt-2"
-                />
-                <BFormInvalidFeedback>
-                  <ValidationErrorMessage :messages="validationErrorMessages.password" />
-                </BFormInvalidFeedback>
-              </div>
-              <div v-if="unauthorizedErrorMessage !== ''" class="text-center mess">
-                {{ unauthorizedErrorMessage }}
-              </div>
-              <SubmitButton
-                :isDisabled="isDisabledButton"
-                :content="'Đăng nhập'"
-                :color="'black'"
-              />
-            </form>
+        <BRow>
+          <BCol>
+            Phần chữ ảnh
           </BCol>
         </BRow>
-        <BRow class="mt-2 mb-5">
-          <BCol class="text-center">
-            <span>Bạn chưa có tài khoản? </span>
-            <NuxtLink to="/register">Đăng ký</NuxtLink>
-          </BCol>
-        </BRow>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
+        <p>asdfasdfasd</p>
       </BContainer>
-    </BContainer>
+  </BContainer>
   </div>
 </template>
 
@@ -68,61 +133,17 @@
 definePageMeta({
   layout: false,
 });
-const {setToken} = useToken();
-const {getConfig} = useConfig();
-const requestBody = reactive({username: '', password: ''});
-const unauthorizedErrorMessage = ref('');
-const isDisabledButton = ref(false);
-const validationErrorMessages = ref({
-});
-const {
-  data,
-  statusCode,
-  onFetchResponse,
-  onFetchError,
-  post,
-} = useFetchApi({
-  requireAuth: false,
-  disableHandleErrorUnauthorized: true,
-})(
-  '/users/login',
-  {immediate: false},
-);
-onFetchResponse(() => {
-  setToken(data.value.token);
-  return navigateTo({name: 'dashboard'});
-});
-onFetchError(() => {
-  if (statusCode.value === getConfig('constants.statusCodes.unauthorized')) {
-    unauthorizedErrorMessage.value = data.value.message;
-    console.log('unauth');
-  } else if (statusCode.value === getConfig('constants.statusCodes.validation')) {
-    validationErrorMessages.value = data.value;
-    console.log('vali');
-  }
-  isDisabledButton.value = false;
-});
-// submit login
-const submit = () => {
-  unauthorizedErrorMessage.value = '';
-  validationErrorMessages.value = {};
-  isDisabledButton.value = true;
-  post(requestBody).json().execute();
-};
+window.scroll(() => {
+  console.log('adf');
+})
 </script>
 <style scoped>
-.full{
-  height: 100vh;
-  background-color: #fc9b9b;
+* {
+  /* color: white; */
 }
-.full>div {
-  position: absolute;
-  bottom: 0;
+.top-dashboard {
+  background-image: url("assets/glass.jpg");
+  min-height:  703px;
 }
-a {
-  color: white;
-}
-.mess{
-  color:red;
-}
+
 </style>
